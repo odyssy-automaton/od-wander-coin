cd3c4a07ab0fe2849fb122d9612a109937921915
dekan <dekanbro@gmail.com>
2018-10-05 13:37:42 -0600

    add rinkeby network

--- truffle.js
+++ truffle.js
@@ -1,5 +1,8 @@
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config()
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.MENOMIC
 
 module.exports = {
   networks: {
@@ -8,5 +11,14 @@ module.exports = {
       port: 9545,
       network_id: '*', // Match any network id
     },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/459626663bcf4ae4bcc5cd9b3c8f9ed6")
+      },
+      network_id: 3
+    }   
+  }
   },
+
+  
 };
