38fa4756fb13367b22235b1c4c2e7542721270ee
oovg <office@vengist.com>
2018-10-30 09:17:59 -0700

    add hrefs to a tags

--- src/components/shared/header/Header.js
+++ src/components/shared/header/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
     return (
       <div className="Header">
         <div>
-          <h1 className="Header__title">Wander Coin</h1>
+          <h1 className="Header__title"><a href="/">Wander Coin</a></h1>
         </div>
       </div>
     );
