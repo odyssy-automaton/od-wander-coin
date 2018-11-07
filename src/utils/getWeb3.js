4617f00e9b6b17386af282c0698ea113a9e162bc
dekan <dekanbro@gmail.com>
2018-11-07 13:43:59 -0700

    add ethereum enable check

--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -62,38 +62,48 @@ export class Web3Info {
     console.log('provider', this.provider);
   }
 
+  checkWeb3andFallback(web3, resolve, reject) {
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask).
+    const alreadyInjected = typeof web3 !== 'undefined';
+
+    if (alreadyInjected) {
+      // Use Mist/MetaMask's provider.
+      web3 = new Web3(web3.currentProvider);
+      console.log('Injected web3 detected.');
+      resolve(web3);
+    } else {
+      // Fallback to localhost if no web3 injection. We've configured this to
+      // use the development console's port by default.
+      // should use infura instead?
+      const provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545');
+      web3 = new Web3(provider);
+      console.log('No web3 instance injected, using Local web3.');
+      resolve(web3);
+    }
+  }
+
   getWeb3() {
     return new Promise((resolve, reject) => {
       // Wait for loading completion to avoid race conditions with web3 injection timing.
+      let web3 = undefined;
       window.addEventListener('load', () => {
         if (window.ethereum) {
           // does this check for new metamask
           // what to do when rejected
           window.ethereum
             .enable()
-            .then((arg1) => console.log('user approves', arg1))
-            .catch((arg1) => console.log('user disaproves', arg1));
-        }
-        let web3 = window.web3;
-
-        // Checking if Web3 has been injected by the browser (Mist/MetaMask).
-        const alreadyInjected = typeof web3 !== 'undefined';
-
-        if (alreadyInjected) {
-          // Use Mist/MetaMask's provider.
-          web3 = new Web3(web3.currentProvider);
-          console.log('Injected web3 detected.');
-          resolve(web3);
+            .then((arg1) => {
+              console.log('user approves', arg1);
+              web3 = window.web3;
+              this.checkWeb3andFallback(web3, resolve, reject);
+            })
+            .catch((arg1) => {
+              console.log('user disaproves', arg1);
+              reject();
+            });
         } else {
-          // Fallback to localhost if no web3 injection. We've configured this to
-          // use the development console's port by default.
-          // should use infura instead?
-          const provider = new Web3.providers.HttpProvider(
-            'http://127.0.0.1:9545',
-          );
-          web3 = new Web3(provider);
-          console.log('No web3 instance injected, using Local web3.');
-          resolve(web3);
+          web3 = window.web3;
+          this.checkWeb3andFallback(web3, resolve, reject);
         }
       });
     });
