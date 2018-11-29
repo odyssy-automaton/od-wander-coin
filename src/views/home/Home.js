332e423832e8345000468036454522a34fd62f5c
dekan <dekanbro@gmail.com>
2018-11-29 08:26:08 -0700

    not working

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 
-import { ClientInfoConsumer } from '../../contexts/ClientInfoContext';
 import WanderingToken from '../../components/wandering';
+import { Web3Consumer } from 'web3-react';
 
 class Home extends Component {
   render() {
     const { tokenId } = this.props.match.params;
 
     return (
-      <ClientInfoConsumer>
+      <Web3Consumer>
         {(context) => (
           <WanderingToken
-            web3={context.web3}
-            account={context.accounts[0]}
+            web3={context.web3js}
+            account={context.account}
             tokenId={tokenId}
           />
         )}
-      </ClientInfoConsumer>
+      </Web3Consumer>
     );
   }
 }
