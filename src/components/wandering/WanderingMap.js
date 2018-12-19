d0ef5799858fb6e13c770d2aa15b7212085ccd17
dekan <dekanbro@gmail.com>
2018-12-19 15:29:21 -0700

    load googlemapsurl in component

--- src/components/wandering/WanderingMap.js
+++ src/components/wandering/WanderingMap.js
@@ -1,48 +1,51 @@
 import React from 'react';
 import { withGoogleMap, GoogleMap, Polyline } from 'react-google-maps';
+import withScriptjs from 'react-google-maps/lib/withScriptjs';
 
 import CoinMarker from './CoinMarker';
 import { defaultLocation } from '../../utils/locationHelpers';
 import { mapStyles } from './mapStyles';
 
-const WanderingMap = withGoogleMap((props) => {
-  const markers = props.tokens.map((token, i) => (
-    <CoinMarker key={i} token={token} tokenKey={i + 1} />
-  ));
+const WanderingMap = withScriptjs(
+  withGoogleMap((props) => {
+    const markers = props.tokens.map((token, i) => (
+      <CoinMarker key={i} token={token} tokenKey={i + 1} />
+    ));
 
-  const centerCoord =
-    props.tokens.length > 0
-      ? props.tokens[props.tokens.length - 1]
-      : defaultLocation();
+    const centerCoord =
+      props.tokens.length > 0
+        ? props.tokens[props.tokens.length - 1]
+        : defaultLocation();
 
-  const setZoom = (ref) => {
-    if (props.tokens.length > 1) {
-      if (!ref) {
-        return;
+    const setZoom = (ref) => {
+      if (props.tokens.length > 1) {
+        if (!ref) {
+          return;
+        }
+        ref.map = ref;
+        var bounds = new window.google.maps.LatLngBounds();
+        props.tokens.forEach((p) => {
+          var latLng = new window.google.maps.LatLng(p.lat, p.lng);
+          bounds.extend(latLng);
+        });
+        ref.map.fitBounds(bounds);
       }
-      ref.map = ref;
-      var bounds = new window.google.maps.LatLngBounds();
-      props.tokens.forEach((p) => {
-        var latLng = new window.google.maps.LatLng(p.lat, p.lng);
-        bounds.extend(latLng);
-      });
-      ref.map.fitBounds(bounds);
-    }
-  };
+    };
 
-  return (
-    <div>
-      <GoogleMap
-        ref={setZoom}
-        defaultOptions={{ styles: mapStyles }}
-        defaultZoom={9}
-        center={centerCoord}
-      >
-        {markers}
-        <Polyline path={props.tokens} options={{ strokeColor: '#5f5fff' }} />
-      </GoogleMap>
-    </div>
-  );
-});
+    return (
+      <div>
+        <GoogleMap
+          ref={setZoom}
+          defaultOptions={{ styles: mapStyles }}
+          defaultZoom={9}
+          center={centerCoord}
+        >
+          {markers}
+          <Polyline path={props.tokens} options={{ strokeColor: '#5f5fff' }} />
+        </GoogleMap>
+      </div>
+    );
+  }),
+);
 
 export default WanderingMap;
